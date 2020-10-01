53184ccd9e0b346de4ef8bbccaf5441721d4fd5a
Evans Kim <ceo.evans@candleworks.co.kr>
2020-10-02 01:51:35 +0900

    기본 구조

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,174 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="relative bg-gray-200 min-h-screen">
+    <div class="flex items-stretch">
+      <div ref="renderer" id="renderer" class="relative w-full bg-white border-gray-200 shadow-md min-h-screen"
+           @drop="dropHandler" @dragover="dragOverHandler">
+        <draggable
+            :list="contents"
+            :disabled="!enabled"
+            class="list-group"
+            ghost-class="ghost"
+            :move="checkMove"
+            @start="dragging = true"
+            @end="dragging = false"
+        >
+          <component v-for="content in contents" :is="content.option.name"
+                     :ref="content.uid"
+                     :key="content.uid"
+                     @content-selected="()=>{setSelectedContent(content)}"
+                     v-bind.sync="content.data"
+                     :class="contentClass(content)"
+          >
+          </component>
+        </draggable>
+
+      </div>
+      <div class="border-l border-gray-200 shadow-md p-4 bg-gray-200 min-h-screen" :style="panelStyle">
+        <div>
+          <ul class='flex cursor-pointer'>
+            <li class='py-2 px-6 bg-white rounded-t-lg' :class="getActivePanelClass('Contents')"
+                @click="()=>{section = 'Contents'}">컨덴츠
+            </li>
+            <li class='py-2 px-6 bg-white rounded-t-lg' :class="getActivePanelClass('Panel')"
+                @click="()=>{section = 'Panel'}">패널
+            </li>
+          </ul>
+        </div>
+        <div class="bg-white p-2" style="min-height: 600px">
+          <component :is="section" @drag:component="setDragComponent" :content="selectedContent"></component>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <toolbar class="hidden"></toolbar>
   </div>
 </template>
+<script>
+import Toolbar from "@/components/Toolbar";
+import Contents from "@/views/Contents";
+import Panel from "@/views/Panel";
+
+export default {
+  name: 'VuePageBuilder',
+  components: {
+    Contents,
+    Toolbar,
+    Panel
+  },
+  data() {
+    return {
+      section: 'Contents',
+      contents: [],
+      contentData: {},
+      dragComponent: null,
+      selectedContent: null,
+      enabled: true,
+      dragging: false,
+    }
+  },
+  computed: {
+    getBinder() {
+      return this.$store.getters.getBind;
+    },
+    panelStyle() {
+      const height = window.innerHeight;
+      return {
+        height: (height - 100) + 'px',
+        width: '360px'
+      }
+    }
+  },
+  methods: {
+    getUniqueName() {
+      return Math.random().toString(36).substr(2, 9);
+    },
+    createContent(component) {
+      const content = {
+        option: component,
+        component: null,
+        uid: this.getUniqueName(),
+        listeners: {},
+      }
+      const props = Object.keys(this.dragComponent.props)
+      this.$set(content, 'data', {});
+
+      props.map((prop) => {
+        const property = this.dragComponent.props[prop];
+        let d = '';
+        if (property.default) {
+          switch (typeof property.default) {
+            case "function":
+              d = property.default();
+              break;
+            default :
+              d = property.default;
+          }
+        }
+        this.$set(content['data'], prop, d)
+      })
+
+      return content;
+    },
+    contentClass(content) {
+      if (this.selectedContent === content) {
+        return ['shadow-outline']
+      }
+    },
+    dropHandler() {
+      if(!this.dragComponent){
+
+        return;
+      }
+      const content = this.createContent(this.dragComponent)
+      this.contents.push(content);
+
+      this.dragComponent = null;
+
+      this.$nextTick(() => {
+        content.component = this.$refs[content.uid][0];
+      })
+    },
+    setSelectedContent(content) {
+      this.selectedContent = content;
+      this.$nextTick(() => {
+        this.section = 'Panel';
+      })
+    },
+    /**
+     *
+     * @param {Event} ev
+     */
+    dragOverHandler(ev) {
+      ev.preventDefault();
+    },
+    setDragComponent(component) {
+      this.dragComponent = component;
+    },
+    getActivePanelClass(name) {
+      return {'text-gray-500': this.section !== name, 'bg-gray-200': this.section !== name}
+    },
+    checkMove(e) {
+      console.log("Future index: " + e.draggedContext.futureIndex);
+    }
+  }
+}
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+.draggable.dragging {
+  opacity: .3;
+  background-color: #a1a2a3;
 }
 
-#nav {
-  padding: 30px;
+.drop-target {
+  box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.inspectable:hover {
+  box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  > * {
+    border: 2px dashed #cccccc;
   }
 }
 </style>
